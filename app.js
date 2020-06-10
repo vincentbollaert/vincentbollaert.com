8a82262df2b44ba07539f895ad3a55d4a0d7152a
Vince <vincentbollaert@gmail.com>
2020-06-10 19:17:23 +0700

    add custom header to custom endpoint

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ app.get('/', (req, res) => {
 	res.send('hello world');
 });
 
+app.get('/custom', (req, res) => {
+	res.set('x-custom-header', 'xxx');
+	res.status(418); // i am a teapot
+	res.send('blahdiblah');
+});
+
 app.listen(port, () => console.log(`app listening on port ${port}`));
